0f8227b52d89d42f4a6cba2f337bd40642ba6090
the pride <85319827+armelnjike@users.noreply.github.com>
2022-06-16 03:43:44 +0100

    Update Apprentissage_artificiel.py

--- Machine learning From scratch/Apprentissage_artificiel.py
+++ Machine learning From scratch/Apprentissage_artificiel.py
@@ -4,7 +4,7 @@ X = TypeVar('X')  # type générique pour représenter un point de données
 
 def split_data(data: List[X], prob: float) -> Tuple[List[X], List[X]]:
     """Split data into fractions [prob, 1 - prob]"""
-    data = data[:]                    # Make a superficiel copy
+    data = data[:]                    # Make superficiel copy
     random.shuffle(data)              # shuffle modifies the list.
     cut = int(len(data) * prob)       # Use prob to find a place to cut
     return data[:cut], data[cut:]     # and split the shuffled list there.
